821545bee2f9d0f8dc6462ef06e3fa1878e15b85
當金將見 <togane@toukin-no-MacBook-Air-10.local>
2016-05-19 00:40:15 +0900

    Initial commit

---
+++ app/src/main/java/zack/inc/jp/studytestproject/MainActivity.java
@@ -0,0 +1,72 @@
+package zack.inc.jp.studytestproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private CameraBridgeViewBase mCameraView;
+
+    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
+    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // 読み込みが成功したらカメラプレビューを開始
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // カメラビューのインスタンスを変数にバインド
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        // リスナーの設定 (後述)
+        mCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 非同期でライブラリの読み込み/初期化を行う
+        // static boolean initAsync(String Version, Context AppContext, LoaderCallbackInterface Callback)
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.gray();
+    }
+}
